d80bb4f6c8b2ae04296d8cae46b8784c9290d70f
Ignazio Montebello <ignaziom86@yahoo.it>
2021-06-09 16:43:48 +0200

    End main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,16 +10,75 @@
 
 {{-- Contenuto del MAIN --}}
 @section("main_section")
-    <div class="bg-black">
+    {{-- Comics section --}}
+    <section class="bg-black">
         <div class="container">
+
+            <div class="series">
+                <a href="#"><h1>Current Series</h1></a>
+            </div>
+            
+
             <div class="boxes">
                 @foreach ($comics as $comic)
-                    {{-- <div class="single-box">
-                        <img src="" alt="Cover"> thumb
-                        <h5>series</h5>
-                    </div> --}}
+                    <div class="single-box">
+                        <div class="img-box">
+                            <img src="{{ $comic["thumb"]}}" alt="{{ $comic["title"]}}">
+                        </div>
+                        <h5>{{ $comic["series"]}}</h5>
+                    </div>
                 @endforeach
             </div>
+            
+            <button>Load more</button>
         </div>
-    </div>
+    </section>
+
+    {{-- Contents section --}}
+    <section class="contents">
+        <div class="container">
+            <ul>
+                <li>
+                    <a href="#">
+                        <div class="icon-img">
+                            <img src="{{ asset("img/buy-comics-digital-comics.png")}}" alt="Digital comics">
+                        </div>
+                        <h5>Digital Comics</h5>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="icon-img">
+                            <img src="{{ asset("img/buy-comics-merchandise.png")}}" alt="Merchandise">
+                        </div>
+                        <h5>Digital Merchandise</h5>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="icon-img">
+                            <img src="{{ asset("img/buy-comics-subscriptions.png")}}" alt="Subscription">
+                        </div>
+                        <h5>Subscription</h5>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="icon-img">
+                            <img src="{{ asset("img/buy-comics-shop-locator.png")}}" alt="Shop locator">
+                        </div>
+                        <h5>Comic shop locator</h5>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div class="icon-img">
+                            <img src="{{ asset("img/buy-dc-power-visa.svg")}}" alt="Visa">
+                        </div>
+                        <h5>DC power visa</h5>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </section>
 @endsection
